8399e0646c9e48c26115579be7c14123085c9af1
vtammineni <venu.tammineni@gmail.com>
2023-02-16 16:37:10 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 550);
+    canvas.position(560, 150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function gotPoses(){
+    
+}
